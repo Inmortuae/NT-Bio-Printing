8d7aea59f38996e41f584ed8a7458fb0e7f3436b
Inmortuae <tophatproductions47@gmail.com>
2025-02-15 21:17:12 -0500

    Changes to be committed: 	modified:   Lua/Scripts/Server/EmptySyringe.lua 	new file:   NTimg/limbs.png 	new file:   NTsound/severed.ogg 	modified:   Translations/English.xml 	modified:   Xml/Items/BioMat.xml 	modified:   Xml/Items/StemCell.xml 	new file:   Xml/Machines/machinelights.png 	new file:   Xml/Machines/organprinter.png 	renamed:    Xml/Printer/organprinter.xml -> Xml/Machines/organprinter.xml 	new file:   Xml/Machines/stemcellprocessor.png 	new file:   Xml/Machines/stemcellprocessor.xml 	new file:   Xml/Overrides/geneoverride.xml 	new file:   Xml/Overrides/limbsoverride.xml 	renamed:    Xml/Printer/organroverride.xml -> Xml/Overrides/organroverride.xml 	deleted:    Xml/Printer/organprinter.png 	modified:   filelist.xml

--- Lua/Scripts/Server/EmptySyringe.lua
+++ Lua/Scripts/Server/EmptySyringe.lua
@@ -4,27 +4,31 @@ Hook.Add("empty_syringe_bone.onUse", function(effect, deltaTime, item, targets, 
     local target = targets[1]
     local char = effect.user
     local targetType = tostring(target)
+    local itemGiven = false -- Flag to track if an item has been given
 
     -- Check if target is either Human or Humanhusk
     if targetType == "Human" then
         if HF.GetSkillRequirementMet(char, "medical", 50) then
             for _, limb in pairs(LimbType) do
-                if HF.HasAfflictionLimb(target, "drilledbones", limb, 1) then
-                    if HF.HasAfflictionLimb(target, "bonemarrowextracted", limb, 100) then return else
-                    HF.AddAfflictionLimb(target, "bonemarrowextracted", limb, 10, char) -- Bone damage from harvest
+                if HF.HasAfflictionLimb(target, "drilledbones", limb, 100) and not HF.HasAfflictionLimb(target, "bonemarrowextracted", limb, 100) then
+                    if not itemGiven then  -- Ensure only one item is given
+                        HF.AddAfflictionLimb(target, "bonemarrowextracted", limb, 10, char) -- Bone damage from harvest
+                        HF.GiveItem(char, "bone_marrow")
+                        HF.RemoveItem(item)
+                        itemGiven = true -- Set flag to true to prevent further items being given
                     end
+                else
+                    return
                 end
             end
-            HF.GiveItem(char, "bone_marrow")
-            HF.RemoveItem(item)
         else
-            -- Failed attempt consequences
-            HF.AddAfflictionLimb(target, "severepain", LimbType.Torso, 40, char) -- Excruciating pain
-            HF.AddAfflictionLimb(target, "bonedamage", LimbType.Torso, 50, char) -- Bone damage risk
-            HF.AddAfflictionLimb(target, "t_fracture", LimbType.Torso, 25, char) -- Potential fracture
-            HF.AddAfflictionLimb(target, "internalbleeding", LimbType.Torso, 45, char) -- Internal hemorrhage
+            -- Failed attempt consequences (only applied once to the torso)
+            local torso = LimbType.Torso
+            HF.AddAfflictionLimb(target, "severepain", torso, 40, char) -- Excruciating pain
+            HF.AddAfflictionLimb(target, "bonedamage", torso, 50, char) -- Bone damage risk
+            HF.AddAfflictionLimb(target, "t_fracture", torso, 25, char) -- Potential fracture
+            HF.AddAfflictionLimb(target, "internalbleeding", torso, 45, char) -- Internal hemorrhage
         end
     end
 
 end)
-
