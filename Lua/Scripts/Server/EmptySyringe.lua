46001ebfd03bcc2705a88679fb06e25938ced4c0
Inmortuae <tophatproductions47@gmail.com>
2025-02-17 04:59:39 -0500

    Changes to be committed: 	modified:   Lua/Scripts/Server/EmptySyringe.lua 	modified:   Xml/Items/EmptySyringe.xml

--- Lua/Scripts/Server/EmptySyringe.lua
+++ Lua/Scripts/Server/EmptySyringe.lua
@@ -2,28 +2,25 @@ Hook.Add("empty_syringe_bone.onUse", function(effect, deltaTime, item, targets, 
 
     -- Local Definitions
     local target = targets[1]
+    local limb = targets[2]
+    local limbtype = limb.type
     local char = effect.user
     local targetType = tostring(target)
 
     -- Check if target is either Human or Humanhusk
     if targetType == "Human" then
-        if HF.GetSkillRequirementMet(char, "medical", 50) then
-            for _, limb in pairs(LimbType) do
-                if HF.HasAfflictionLimb(target, "drilledbones", limb, 1) and not HF.HasAfflictionLimb(target, "bonemarrowextracted", limb, 100) then
-                    HF.AddAfflictionLimb(target, "bonemarrowextracted", limb, 20, char) -- Bone damage from harvest
-                    HF.GiveItem(char, "bone_marrow")
-                    HF.RemoveItem(item)
-                    print("Bone marrow extracted from limb")
-                    break -- Ensures only one item is given
-                end
+        if HF.GetSkillRequirementMet(char, "medical", 50) then   
+            if HF.HasAfflictionLimb(target, "drilledbones", limbtype, 1) and not HF.HasAfflictionLimb(target, "bonemarrowextracted", limbtype, 100) then
+                HF.AddAfflictionLimb(target, "bonemarrowextracted", limbtype, 20, char) -- Bone damage from harvest
+                HF.GiveItem(char, "bone_marrow")
+                HF.RemoveItem(item)
+                print("Bone marrow extracted from limb")
             end
         else
-            -- Failed attempt consequences (only applied once to the torso)
-            local torso = LimbType.Torso
-            HF.AddAfflictionLimb(target, "severepain", torso, 40, char) -- Excruciating pain
-            HF.AddAfflictionLimb(target, "bonedamage", torso, 50, char) -- Bone damage risk
-            HF.AddAfflictionLimb(target, "t_fracture", torso, 25, char) -- Potential fracture
-            HF.AddAfflictionLimb(target, "internalbleeding", torso, 45, char) -- Internal hemorrhage
+            HF.AddAfflictionLimb(target, "severepain", limbtype, 40, char) -- Excruciating pain
+            HF.AddAfflictionLimb(target, "bonedamage", limbtype, 50, char) -- Bone damage risk
+            HF.AddAfflictionLimb(target, "t_fracture", limbtype, 25, char) -- Potential fracture
+            HF.AddAfflictionLimb(target, "internalbleeding", limbtype, 45, char) -- Internal hemorrhage
         end
     end
 
